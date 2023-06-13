86a9cf205f597e4644bb70a1d47b30dd12499caf
Dolan <dolware@cumallover.me>
2023-06-13 05:27:50 -0400

    Test

--- src/main/java/dankestsmp/dankestsmpplugin/DankestSMPPlugin.java
+++ src/main/java/dankestsmp/dankestsmpplugin/DankestSMPPlugin.java
@@ -7,7 +7,7 @@ public final class DankestSMPPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+    	// - Tony likes dick in his ass on a daily yuh
     }
 
     @Override
